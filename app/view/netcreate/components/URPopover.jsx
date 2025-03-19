e0da3fd2aaab098dae82e8efa60f55868a193031
benloh <ben@inquirium.net>
2025-03-19 10:03:11 -0700

    wcag: Add URPopover component (used by NCHelpPanel)

---
+++ app/view/netcreate/components/URPopover.jsx
@@ -0,0 +1,52 @@
+/*//////////////////////////////// ABOUT \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*\
+
+  Generic Floating, Draggable, Closeable Window
+
+  USE:
+
+    <URPopover
+      dialog={CMTSTATUS.dialog}
+    />
+
+\*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ * //////////////////////////////////////*/
+
+import React, { useState } from 'react';
+import Draggable from 'react-draggable';
+
+/// CONSTANTS & DECLARATIONS //////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const DBG = false;
+const PR = 'URPopover';
+
+/// REACT FUNCTIONAL COMPONENT ////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+function URPopover({ title, onClose, children }) {
+  /// COMPONENT RENDER ////////////////////////////////////////////////////////
+  /// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  const BTN_CLOSE = (
+    <button
+      className="icon"
+      type="button"
+      role="button"
+      aria-label="Close"
+      onClick={onClose}
+    >
+      <img src="images/icn_plus.svg" />
+    </button>
+  );
+  return (
+    <Draggable>
+      <div id="popover">
+        <div className="popover-toolbar">
+          {title}
+          {BTN_CLOSE}
+        </div>
+        <div className="popover-content">{children}</div>
+      </div>
+    </Draggable>
+  );
+}
+
+/// EXPORT REACT COMPONENT ////////////////////////////////////////////////////
+/// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+export default URPopover;
